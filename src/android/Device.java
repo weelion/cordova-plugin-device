cc74bfa660eab5168bd49109fa5f27b45f0e727f
weelion <weelion@qq.com>
2014-05-08 10:55:54 +0800

    android add imei

--- src/android/Device.java
+++ src/android/Device.java
@@ -29,6 +29,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.provider.Settings;
+import android.telephony.TelephonyManager;
 
 public class Device extends CordovaPlugin {
     public static final String TAG = "Device";
